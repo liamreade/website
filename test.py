0f0798422b6a89d52d50c510dabaca11b68c9c46
liamreade <liamrreade@gmail.com>
2022-10-16 11:11:58 -0400

    Minor modifications

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-print("Hello World!!!")
+#Variables
+x = 1
+y = "alpha"
+z = 3.14
+
+#Data Types
+#There are 3 major data types, integers (int), strings (str), and floats (float)
+
+x_type = type(x)
+print(x_type)
+
+y_type = type(y)
+print(y_type)
+
+z_type = type(z)
+print(z_type)
+
+#Comments are just hashtags infront of the comment
+
+#Conditional statements
+
+if x == 1:
+    print("Yes ofc")
